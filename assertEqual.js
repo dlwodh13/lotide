ceabfcb4cad14c7b2da9ae3a7c3485578eb54d26
Jea Oh Lee <jeaohlee99@gmail.com>
2022-09-19 19:21:45 -0700

    Assert equal

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse", "Bootcamp");
+assertEqual(1, 1);
