3297b19103673962a4d28ba219914b9c6076bb68
Jea Oh Lee <jeaohlee99@gmail.com>
2022-09-20 17:31:40 -0700

    Implement tail - Lotide Assignment

--- tail.js
+++ tail.js
@@ -1,18 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫 Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   let tempArr = [];
   for(i = 1; i < arr.length; i++){
     tempArr.push(arr[i]);
   }
   return tempArr;
-}
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.toString(), ["Lighthouse", "Labs"].toString()); // => will always fail!
+};
