4d034eca8ca05ac29fe4668fe026422c5e85f8a9
Jea Oh Lee <jeaohlee99@gmail.com>
2022-09-22 17:27:42 -0700

    implement findKey - lotide challenge

---
+++ findKey.js
@@ -0,0 +1,30 @@
+//const assertEqual = require('./assertEqual.js');
+
+const findKey = function(object, callback) {
+  for(let element in object) {
+    if(callback(object[element])){
+      return element;
+    }
+  }
+}
+
+module.exports = findKey;
+
+// findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2); // => "noma"
+
+// assertEqual(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2),'noma');
+
